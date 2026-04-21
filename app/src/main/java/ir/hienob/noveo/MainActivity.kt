6f8521e07694bbafb90ec60c27af9317169400ca
bargmy <76166212+bargmy@users.noreply.github.com>
2026-04-21 18:04:44 +0330

    Stop drawing Noveo UI under system status bar

--- app/src/main/java/ir/hienob/noveo/MainActivity.kt
+++ app/src/main/java/ir/hienob/noveo/MainActivity.kt
@@ -3,7 +3,6 @@ package ir.hienob.noveo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
@@ -15,7 +14,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             val state by viewModel.uiState.collectAsState()
             NoveoRoot(
