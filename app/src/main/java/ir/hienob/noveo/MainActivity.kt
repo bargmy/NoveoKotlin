e82507c5256d4ecfe9923d842c4247e4c43e114c
bargmy <76166212+bargmy@users.noreply.github.com>
2026-04-22 08:26:50 +0330

    Wire direct chat opening from home surfaces

--- app/src/main/java/ir/hienob/noveo/MainActivity.kt
+++ app/src/main/java/ir/hienob/noveo/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import ir.hienob.noveo.app.AppViewModel
 import ir.hienob.noveo.ui.NoveoRoot
 
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                 onAuthMode = viewModel::setAuthMode,
                 onAuthSubmit = viewModel::authenticate,
                 onOpenChat = viewModel::openChat,
+                onStartDirectChat = viewModel::openDirectChat,
                 onBackToChats = viewModel::backToChatList,
                 onSend = viewModel::sendMessage,
                 onLogout = viewModel::logout
